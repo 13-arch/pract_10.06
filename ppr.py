fea53fa73750af6c606339cef1a9032944e9b59e
13-arch <Iron_Mike@tutamail.com>
2025-06-12 11:28:04 +0300

    xxx

--- ppr.py
+++ ppr.py
@@ -5,7 +5,12 @@ import unittest
 #тестируемая часть приложения
 
 def round_square(r):        #Функция для поиска площади
-    square=r*r*3.14
+    if isinstance(r,str) is True:
+        square="TypeError"
+    elif r<0:
+        square="ValueError: radius cant be negative"
+    else:
+        square=r*r*3.14
     return square
 
     
@@ -26,12 +31,12 @@ class TestRoundSquare(unittest.TestCase):
         self.assertEqual(round_square(0), 0.0)
 
     def test_float_positive_numbers(self):
-        self.assertEqual(round_square(0.1), 0.314)
+        self.assertEqual(round_square(0.1), 0.031400000000000004 )
     
     def test_error_input(self):
-        self.assertEqual(round_square('dsfsdf'), TypeError)
-        self.assertEqual(round_square(''), TypeError)
-        self.assertEqual(round_square(10**-9), TypeError)
+        self.assertEqual(round_square('dsfsdf'), "TypeError")
+        self.assertEqual(round_square(''), "TypeError")
+        self.assertEqual(round_square("10**-9"), "TypeError")
 
 if __name__ == "__main__":
     unittest.main()
